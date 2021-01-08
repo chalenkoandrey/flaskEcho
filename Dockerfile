dee77d90dd189e7b2411dec3d07a0bed9dc75fbf
Andrii <andrysha1999@gmail.com>
2021-01-08 14:21:34 +0200

    works

--- Dockerfile
+++ Dockerfile
@@ -2,7 +2,7 @@ FROM python:3.7
 
 RUN mkdir /app
 WORKDIR /app
-COPY . /app/
+ADD . /app/
 RUN pip install -r requirements.txt
 
 EXPOSE 5000
--- main.py
+++ main.py
@@ -1,23 +1,29 @@
 from flask import Flask
 from flask import render_template
-app = Flask(__name__)
+import os
 import mysql.connector
+app = Flask(__name__)
+host="database-2.cyo3bhhkjl95.ca-central-1.rds.amazonaws.com"
+try:
+  mydb = mysql.connector.connect(
+    host="database-2.cyo3bhhkjl95.ca-central-1.rds.amazonaws.com",
+    user="admin",
+    password="12345678"
+  )
+  mycursor = mydb.cursor()
 
-mydb = mysql.connector.connect(
-  host="database-1.cepq1lphbhoy.eu-west-1.rds.amazonaws.com",
-  user="admin",
-  password="12345678"
-)
+  mycursor.execute("select version()")
 
-mycursor = mydb.cursor()
+  res=mycursor.fetchall()
+  version=res
+except:
+   print("No connect to db")
+   version="No_connect_to_db"
 
-mycursor.execute("select version()")
-
-res=mycursor.fetchall()
 @app.route('/')
 @app.route('/index')
 def hello():
-   return render_template("index.html",version=res)
+   return render_template("index.html",version=version,host=host)
 
 
 if __name__ == "__main__":
